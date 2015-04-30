ba6916424b9313c7ef964a6fa5bc91b6435fc44b
Hugo Klepsch <hugo.klepsch@gmail.com>
2015-04-29 21:09:19 -0400

    Finished commenting questions.shortanswer

--- GUITestMaker/src/questions/ShortAnswer.java
+++ GUITestMaker/src/questions/ShortAnswer.java
@@ -2,9 +2,8 @@
 		 Title: ShortAnswer.java
 		 Programmer: hugo
 		 Date of creation: Apr 20, 2015
-		 Description: 
-*/
-
+		 Description: class to hold the all the information required to hold a short answer question
+ */
 
 package questions;
 
@@ -13,29 +12,28 @@ package questions;
  *
  */
 public class ShortAnswer {
-	String type = "SA";
+	String type = "SA"; // short identifier for easy identification in the quiz.quiz file
 	String question;
 	String answer;
-	int quesNum;
-	
-	
-	
-	
+	int quesNum; // used to help order the questions when taking the test.
+
 	/**
 	 * @param question is the question
 	 * @param answer is the answer
+	 * ^^^^^ Graham please I think that is obvious. 
 	 */
+
 	public ShortAnswer(String question, String answer) {
 		super();
 		this.question = question;
 		this.answer = answer;
 	}
-	
+
 	/**
 	 * 
 	 * @param question is the question
 	 * @param answer is the answer
-	 * @param quesNum is the question number identifier
+	 * @param quesNum is the question number identifier, used to take the questions in the same order that they are made. 
 	 */
 	public ShortAnswer(String question, String answer, int quesNum) {
 		super();
@@ -43,31 +41,35 @@ public class ShortAnswer {
 		this.answer = answer;
 		this.quesNum = quesNum;
 	}
+
 	/**
 	 * @return the question
 	 */
 	public String getQuestion() {
 		return question;
 	}
+
 	/**
-	 * @param question the question to set
+	 * @param set the question
 	 */
 	public void setQuestion(String question) {
 		this.question = question;
 	}
+
 	/**
 	 * @return the answer
 	 */
 	public String getAnswer() {
 		return answer;
 	}
+
 	/**
-	 * @param answer the answer to set
+	 * @param set the answer
 	 */
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-	
+
 	/**
 	 * 
 	 * @return the question number
@@ -75,6 +77,7 @@ public class ShortAnswer {
 	public int getQuesNum() {
 		return quesNum;
 	}
+
 	/**
 	 * 
 	 * @param quesNum is the question number
@@ -82,28 +85,33 @@ public class ShortAnswer {
 	public void setQuesNum(int quesNum) {
 		this.quesNum = quesNum;
 	}
+
 	/**
 	 * @author Graham
 	 * @param answer the user's answer to check
 	 * @return true if correct, false if incorrect
 	 */
-	public boolean checkAnswer(String answer){
-		if(answer.equalsIgnoreCase(this.answer))
+	public boolean checkAnswer(String answer) {
+		if (answer.equalsIgnoreCase(this.answer)) // use ignore case to ensure that it doesn't matter the case of the
+													// answer.
 			return true;
-		
+
 		else
 			return false;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
+		/*
+		 * this method is used to give an attractive output on the screen after the user has added the question. 
+		 */
 		String question = "Question: " + this.question + "\n";
 		String answer = "Answer: " + this.answer + "\n";
 		return question + answer;
 	}
-	
-	
+
 }
